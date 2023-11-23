948d7258750e547d6776c0156bfbd98c7b278bf7
Alisher Tungyshbay <www.at.7777@gmail.com>
2023-11-24 01:37:06 +0600

    Final v2

--- js/form_validation.js
+++ js/form_validation.js
@@ -37,20 +37,6 @@ function validatePassword(passwordInput, passwordError) {
     return true;
 }
 
-function validateConfirmPassword(passwordInput, confirmPasswordInput, confirmPasswordError) {
-    var password = passwordInput.value;
-    var confirmPassword = confirmPasswordInput.value;
-
-    if (password !== confirmPassword) {
-        confirmPasswordError.textContent = "Passwords do not match.";
-        confirmPasswordError.style.display = "block";
-        return false;
-    }
-
-    confirmPasswordError.style.display = "none";
-    return true;
-}
-
 function validateLoginForm() {
     var emailInput = document.querySelector(".login .input[type='email']");
     var emailError = document.querySelector(".login .email-error");
@@ -60,27 +46,129 @@ function validateLoginForm() {
     return validateEmail(emailInput, emailError) && validatePassword(passwordInput, passwordError);
 }
 
-function validateSignupForm() {
-    var emailInput = document.querySelector(".signup .input[type='email']");
-    var emailError = document.querySelector(".signup .email-error");
-    var passwordInput1 = document.querySelector(".signup .input[type='password']");
-    var passwordError1 = document.querySelector(".signup .password-error:first-of-type");
-    var passwordInput2 = document.querySelectorAll(".signup .input[type='password']")[1];
-    var passwordError2 = document.querySelector(".signup .password-error:last-of-type");
+var currentTab = 0;
+showTab(currentTab);
 
-    var isValid = true;
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    fixStepIndicator(n)
+}
 
-    if (!validateEmail(emailInput, emailError)) {
-        isValid = false;
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length - 1) {
+        document.getElementById("reloadMessage").style.display = "block";
+
+        setTimeout(function () {
+            move();
+        }, 1800)
+
+        setTimeout(function () {
+            document.getElementById("reloadMessage2").style.display = "block";
+        }, 2400)
+
+        setTimeout(function () {
+            location.reload();
+        }, 3000);
+        return false;
+    }
+    showTab(currentTab);
+}
+
+function validateInput(input, pattern, errorId) {
+    var errorMessageElement = document.getElementById(errorId);
+    if (!pattern.test(input.value)) {
+        switch (errorId) {
+            case 'errorFirstName':
+                errorMessageElement.innerHTML = "First name should contain at least 4 letters";
+                break;
+            case 'errorLastName':
+                errorMessageElement.innerHTML = "Last name should contain at least 4 letters";
+                break;
+            case 'errorEmail':
+                errorMessageElement.innerHTML = "Enter a valid email address";
+                break;
+            case 'errorPhone':
+                errorMessageElement.innerHTML = "Enter a valid 11-digit phone number";
+                break;
+            case 'errorLogin':
+                errorMessageElement.innerHTML = "Use only letters and least 6 characters, numbers, and underscores for the login";
+                break;
+            case 'errorPassword':
+                errorMessageElement.innerHTML = "Password should contain at least one lowercase letter, one uppercase letter, one digit, and be at least 8 characters long";
+                break;
+            default:
+                errorMessageElement.innerHTML = "Invalid input";
+                break;
+        }
+
+        input.className = 'invalid';
+    } else {
+        errorMessageElement.innerHTML = "";
+        input.className = '';
+    }
+}
+
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+
+    var patterns = {
+        "fname": /^[a-zA-Z]{4,}$/,
+        "lname": /^[a-zA-Z]{4,}$/,
+        "email": /^\S+@\S+\.\S+$/,
+        "phone": /^\d{11,}$/,
+        "login": /^[a-zA-Z0-9_]{6,}$/,
+        "password": /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
+    };
+
+    for (i = 0; i < y.length; i++) {
+        var inputType = y[i].placeholder.toLowerCase();
+        var pattern = patterns[inputType] || /^[a-zA-Z]+$/;
+
+        if (!pattern.test(y[i].value)) {
+            y[i].className += " invalid";
+            valid = false;
+        }
     }
 
-    if (!validatePassword(passwordInput1, passwordError1)) {
-        isValid = false;
-    }
+    return valid;
+}
 
-    if (!validateConfirmPassword(passwordInput1, passwordInput2, passwordError2)) {
-        isValid = false;
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
     }
+    x[n].className += " active";
+}
 
-    return isValid;
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 3);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
 }
---
+++ js/search.js
@@ -0,0 +1,45 @@
+function searchPage() {
+    var searchInput = document.getElementById('searchInput');
+    var searchTerm = searchInput.value.toLowerCase();
+
+    if (searchTerm.trim() === "") {
+        return false;
+    }
+
+    var pageText = document.body.innerText.toLowerCase();
+    var searchIndex = pageText.indexOf(searchTerm);
+
+    if (searchIndex !== -1) {
+        window.scrollTo(0, getOffsetTop(pageText, searchIndex));
+        
+        highlightText(searchTerm);
+    } else {
+        alert("No matching results found.");
+    }
+
+    return false;
+}
+
+function highlightText(searchTerm) {
+    var pageText = document.body.innerHTML;
+    var highlightedText = '<span style="background-color: yellow;">' + searchTerm + '</span>';
+    var regex = new RegExp(searchTerm, 'gi');
+    var highlightedPageText = pageText.replace(regex, highlightedText);
+    document.body.innerHTML = highlightedPageText;
+}
+
+
+function getOffsetTop(text, index) {
+    var linesBefore = text.substr(0, index).split('\n');
+    var lineCount = linesBefore.length - 1; 
+    var lineHeight = parseFloat(getComputedStyle(document.body).lineHeight);
+    return lineCount * lineHeight;
+}
+
+document.getElementById('searchInput').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        searchPage();
+    }
+});
+
